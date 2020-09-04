016cb0fc6f1bc051f9f3895c528fe11f715170f6
ericwugo <ericwugo@gmail.com>
2020-09-04 11:28:37 +0800

    Initial Commit

---
+++ CalculateEricApp1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CalculateEricApp1
+//
+//  Created by user on 2020/9/4.
+//  Copyright © 2020 user. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
